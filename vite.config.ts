46ecd7f4b82c138ecc7bbd9558fe6dbee70426c6
Ra1NuX <ra1nux.01@gmail.com>
2025-02-15 01:41:47 +0100

    vercel configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     headers: {
-      'Cross-Origin-Opener-Policy': 'same-origin',
       'Cross-Origin-Embedder-Policy': 'require-corp',
+      'Cross-Origin-Opener-Policy': 'same-origin',
     },
   },
   plugins: [react()],
