0d3a5a14dd54e7efc01107023bb4c515df68e90b
Ra1NuX <ra1nux.01@gmail.com>
2025-02-18 23:16:23 +0100

    fix: channel update error

--- electron-builder.config.js
+++ electron-builder.config.js
@@ -16,7 +16,9 @@ const config = {
     owner: 'ra1nux',
     repo: 'OpenTS',
     channel: process.env.CHANNEL === 'beta' ? 'beta' : 'latest',
+    releaseType: process.env.CHANNEL === 'beta' ? 'prerelease' : 'release',
     publishAutoUpdate: true,
+
   },
   artifactName: process.env.CHANNEL === 'beta' ? 'Setup-${productName}${version}.${ext}' : 'Setup-${productName}${version}.${ext}',
   win: {
