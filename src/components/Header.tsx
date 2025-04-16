a21590a9759087e4e5cfc0d845a65449a3d23cc2
Ra1NuX <ra1nux.01@gmail.com>
2025-04-16 23:51:22 +0200

    feat: add new icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -19,7 +19,7 @@ const Header = () => {
   return (
     <nav ref={header} className="h-10 dark:bg-main-dark bg-[#f7f7f7] drag z-10 p-1.5">
       <div className="md:w-4/12 w-full h-full leading-[30px] dark:text-[#f7f7f7] float-left pl-2 flex items-center gap-2 ">
-        <img src="/icon.png" className="md:h-[20px] h-8 inline-block" />
+        <img src="/icon.png" className="md:h-[25px] h-8 inline-block mt-1" />
         <p className="md:text-sm text-xl font-medium mt-0.5">
           OpenTS <span className="font-light dark:text-[#f7f7f740] text-main-dark/60 italic"> Beta</span>
         </p>
